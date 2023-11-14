345ba55884663e4f3e7747a6807fd493a77742a5
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2023-11-14 02:00:36 +0100

    add swagger docs for PUT request ✅

--- routes/books.js
+++ routes/books.js
@@ -131,6 +131,38 @@ router.post('/', (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /books/{id}:
+ *  put:
+ *    summary: Update the book by the id
+ *    tags: [Books]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Book'
+ *    responses:
+ *      200:
+ *        description: The book was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Book'
+ *      404:
+ *        description: The book was not found
+ *      500:
+ *        description: Some error happened
+ */
+
 router.put('/:id', (req, res) => {
   try {
     req.app.db.get('books').find({ id: req.params.id }).assign(req.body).write();
