d9a670fa8908ee74a4952103e5b50fe1c87d2d4a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2023-11-14 01:36:56 +0100

    add mock db 📝

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const low = require("lowdb");
 
 const PORT = process.env.PORT || 8080;
 
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ books: [] }).write();
+
 const app = express();
+
+app.db = db;
 
 app.use(cors());
 app.use(express.json());
