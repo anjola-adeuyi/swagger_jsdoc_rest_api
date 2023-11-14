1401f800104cc70921e23c676fbfed1740a447c3
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2023-11-14 01:57:51 +0100

    add swagger jsdoc for GET request ✅

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,55 @@ const { nanoid } = require('nanoid');
 
 const idLength = 8;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: j5fE_Trw
+ *         title: How to think like a man
+ *         author: Barack Obama
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
+
 router.get('/', (req, res) => {
   const books = req.app.db.get('books');
 
