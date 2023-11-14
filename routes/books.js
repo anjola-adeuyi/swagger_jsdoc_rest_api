8726aece1327a84fa533e16c2bedd2e3b31d0ed7
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2023-11-14 01:58:38 +0100

    add swagger docs GET /:id ✅

--- routes/books.js
+++ routes/books.js
@@ -59,6 +59,30 @@ router.get('/', (req, res) => {
   res.send(books);
 });
 
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Get the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: The book was not found
+ */
+
 router.get('/:id', (req, res) => {
   const book = req.app.db.get('books').find({ id: req.params.id }).value();
 
